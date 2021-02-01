acff8411626795a4a47d5b1ff861a9ae9eb21619
Phaeon <46031169+Phaeon@users.noreply.github.com>
2021-02-01 13:54:21 +0100

    Update creation_db.sql
    Modifications des bases

--- scripts_sql/creation_db.sql
+++ scripts_sql/creation_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE convocations IF NOT EXISTS;
 
-DROP TABLE IF EXISTS Equipes;
+DROP TABLE IF EXISTS Effectifs;
 DROP TABLE IF EXISTS Sites;
 DROP TABLE IF EXISTS Competitions;
 DROP TABLE IF EXISTS NonConvoques;
@@ -12,8 +12,7 @@ DROP TABLE IF EXISTS Absences;
 CREATE TABLE Effectifs (
     nom VARCHAR(30),
     prenom VARCHAR(30),
-    type_licence VARCHAR(20) NOT NULL,
-    CONSTRAINT c_licence CHECK (type_licence IN ('Libre', 'Futsal', 'Entreprise', 'Loisir')),
+    type_licence VARCHAR(20) NOT NULL CHECK (type_licence IN ('Libre', 'Futsal', 'Entreprise', 'Loisir')),
     PRIMARY KEY (nom, prenom)
 );
 
@@ -21,9 +20,8 @@ CREATE TABLE Effectifs (
 CREATE TABLE Competitions (
     nom_compet VARCHAR(40),
     -- Permet de définir l'importance de la compétition par un numéro quelconque (facultatif)
-    importance INTEGER,
-    PRIMARY KEY (nom_compet),
-    CONSTRAINT c_importance CHECK (importance >= 0)
+    importance INTEGER CHECK (importance >= 0),
+    PRIMARY KEY (nom_compet)
 );
 
 
@@ -38,10 +36,9 @@ CREATE TABLE Sites (
 CREATE TABLE NonConvoques (
     nom VARCHAR(30),
     prenom VARCHAR(30),
-    raison VARCHAR(15) NOT NULL,
+    raison VARCHAR(15) NOT NULL CHECK (raison IN ('Exempt', 'Absent', 'Blesse', 'Suspendu', 'Sans licence')),
     PRIMARY KEY (nom, prenom),
-    FOREIGN KEY (nom, prenom) REFERENCES Effectifs(nom, prenom),
-    CONSTRAINT c_raison CHECK (raison IN ('Exempt', 'Absent', 'Blesse', 'Suspendu', 'Sans licence'))
+    FOREIGN KEY (nom, prenom) REFERENCES Effectifs(nom, prenom)
 );
 
 
@@ -53,52 +50,46 @@ CREATE TABLE Equipes (
 );
 
 
-CREATE TABLE Absences (
-    nom VARCHAR(30),
-    prenom VARCHAR(30),
-    date_m DATE,
-    raison VARCHAR(15) NOT NULL,
-    raison_court CHAR(1) NOT NULL,
-    PRIMARY KEY (nom, prenom, date_m),
-    FOREIGN KEY (nom, prenom) REFERENCES Effectifs(nom, prenom),
-    FOREIGN KEY (date_m) REFERENCES Convocations(date_m),
-    FOREIGN KEY (raison) REFERENCES NonConvoques(raison),
-    CONSTRAINT c_raison_court CHECK (raison_court IN ('A', 'B', 'N', 'S'))
-);
-
-
 CREATE TABLE Matchs (
-    categorie VARCHAR(20) NOT NULL,
-    competition VARCHAR(20) NOT NULL,
-    equipe VARCHAR(20) NOT NULL,
-    equipe_adv VARCHAR(50) NOT NULL,
-    date_m DATE NOT NULL,
-    heure TIME NOT NULL,
-    terrain VARCHAR(50),
+    categorie VARCHAR(20),
+    competition VARCHAR(20),
+    equipe VARCHAR(20),
+    equipe_adv VARCHAR(20),
+    date_m DATE,
+    heure TIME,
     site VARCHAR(50) NOT NULL,
+    terrain VARCHAR(50) NOT NULL,
     PRIMARY KEY (categorie, competition, equipe, equipe_adv, date_m, heure),
-    FOREIGN KEY (categorie) REFERENCES Equipes(categorie),
-    FOREIGN KEY (equipe) REFERENCES Equipes(nom_equipe),
-    FOREIGN KEY (equipe_adv) REFERENCES Equipes(nom_equipe),
-    FOREIGN KEY (terrain) REFERENCES Sites(terrain),
-    FOREIGN KEY (site) REFERENCES Sites(nom_site)
+    FOREIGN KEY (competition) REFERENCES Competitions(nom_compet),
+    FOREIGN KEY (categorie, equipe) REFERENCES Equipes(nom_equipe, categorie),
+    FOREIGN KEY (categorie, equipe_adv) REFERENCES Equipes(nom_equipe, categorie),
+    FOREIGN KEY (site, terrain) REFERENCES Sites(nom_site, terrain)
 );
 
 
 CREATE TABLE Convocations (
-    date_m DATE NOT NULL,
-    competition VARCHAR(20) NOT NULL,
-    equipe_adv VARCHAR(50) NOT NULL,
+    categorie VARCHAR(20),
+    date_m DATE,
+    competition VARCHAR(20),
+    equipe_adv VARCHAR(20),
     site VARCHAR(50) NOT NULL,
-    terrain VARCHAR(50),
-    heure TIME NOT NULL,
+    terrain VARCHAR(50) NOT NULL,
+    heure TIME,
     rdv VARCHAR(100),
-    equipe VARCHAR(20) NOT NULL,
+    equipe VARCHAR(20),
     PRIMARY KEY (date_m, competition, equipe_adv, heure, equipe),
-    FOREIGN KEY (date_m) REFERENCES Matchs(date_m),
-    FOREIGN KEY (competition) REFERENCES Matchs(competition),
-    FOREIGN KEY (equipe_adv) REFERENCES Equipes(nom_equipe),
-    FOREIGN KEY (equipe) REFERENCES Equipes(nom_equipe),
-    FOREIGN KEY (site, terrain) REFERENCES Sites(nom_site, terrain),
-    FOREIGN KEY (heure) REFERENCES Matchs(heure)
+    FOREIGN KEY (categorie, competition, equipe, equipe_adv, date_m, heure) REFERENCES Matchs(categorie, competition, equipe, equipe_adv, date_m, heure),
+    FOREIGN KEY (site, terrain) REFERENCES Sites(nom_site, terrain)
+);
+
+
+CREATE TABLE Absences (
+    nom VARCHAR(30),
+    prenom VARCHAR(30),
+    date_m DATE,
+    raison VARCHAR(15) NOT NULL CHECK (raison IN ('Exempt', 'Absent', 'Blesse', 'Suspendu', 'Sans licence')),
+    raison_court CHAR(1) NOT NULL CHECK (raison_court IN ('A', 'B', 'N', 'S')),
+    PRIMARY KEY (nom, prenom, date_m),
+    FOREIGN KEY (nom, prenom) REFERENCES Effectifs(nom, prenom),
+    FOREIGN KEY (date_m) REFERENCES Convocations(date_m)
 );
